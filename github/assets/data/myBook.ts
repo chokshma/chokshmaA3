bcbc841bd034788ee1e87ac86ec5d267b2ce484f
chokshma <52579109+chokshma@users.noreply.github.com>
2019-07-25 20:39:58 -0400

    Delete myBook.ts

--- github/assets/data/myBook.ts
+++
@@ -1,8 +0,0 @@
-import {BookInfo} from '../../app/bookInfo';
-export const MYBOOKS: BookInfo[] =
-[
-    {bName: '7 Habits of highly effective people', bAuthor: 'Stephen R. Covey', bGenre:'Self-Help',yPublished: '1989', bpicture:'b1.png'},
-    {bName: 'The secret of the nagas', bAuthor: 'Amish Tripathi', bGenre:'Fiction',yPublished: '2011', bpicture:'b2.png'},
-    {bName: 'A Brief History of Time', bAuthor: 'Stephan Hawking', bGenre:'Science',yPublished: '1988', bpicture:'b3.png'},
-    {bName: 'The Woman in the Window', bAuthor: 'Dan Mallory', bGenre:'Suspense',yPublished: '2017', bpicture:'b4.png'}
-]
